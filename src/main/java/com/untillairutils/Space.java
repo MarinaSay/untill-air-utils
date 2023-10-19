23c149a5f9d07cd9f694c798856b4ecdc84a1fb9
MarinaSay <smvbox@gmail.com>
2023-10-19 09:16:17 +0200

    added WebDriverManager

---
+++ src/main/java/com/untillairutils/Space.java
@@ -0,0 +1,20 @@
+package com.untillairutils;
+
+public class Space {
+    public String name;
+
+    public String numberOfTables;
+
+    public Space(String name, String numberOfTables ){
+        this.name=name;
+        this.numberOfTables=numberOfTables;
+    }
+
+    public static Space[] inputSpaces (){
+        Space[] spaces = new Space[3];
+        spaces[0] = new Space ("Restaurant","10" );
+        spaces[1] = new Space ("Bar", "6");
+        spaces[2] = new Space ("Terrace", "5");
+        return spaces;
+    }
+}
